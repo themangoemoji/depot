b8b7ee49ed9747ec834b1d6536f5817e1dde71e1
Michael Wright <wrigh517@gmail.com>
2014-06-24 18:35:55 -0400

    Updated tests for products

--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -4,4 +4,32 @@ class ProductTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+test "product attributes must not be empty" do
+    product = Product.new
+    assert product.invalid?
+    assert product.errors[:title].any?
+    assert product.errors[:description].any?
+    assert product.errors[:price].any?
+    assert product.errors[:image_url].any?
+end
+
+test "product price must be positive" do
+    product = Product.new(title:        "This Title Is The Shit",
+                          description:  "This description is the actual titties",
+                                image_url:  "boob.jpg")
+    product.price = -1
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01",
+        product.errors[:price].join('; ')
+
+    product.price = 0
+    assert product.invalid?
+    assert_equal "must be greater than or equal to 0.01, brah.",
+        product.errors[:price].join('; ')
+    
+    product.price = 1
+    assert product.valid?
+    assert_equal "you did it, doooooood."
+
+end
 end
